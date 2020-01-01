7d2a68503b2f247145671e797284cfdc655168b1
Kamshat Mussina <mussinak777@gmail.com>
2019-12-31 17:43:49 -0700

    getting responses for all languages from third-part APIs, api-route for signup is working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,71 @@
 var db = require("../models");
+var passport = require("../config/passport");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
+  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+    res.json(req.user);
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  app.post("/api/signup", function(req, res) {
+    db.User.create({
+      email: req.body.email,
+      password: req.body.password
+    })
+      .then(function() {
+        res.redirect(307, "/api/login");
+      })
+      .catch(function(err) {
+        res.status(401).json(err);
+      });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
+  });
+
+  app.get("/api/dashboard", function(req, res) {
+    if (!req.user) {
+      res.json({});
+    } else {
+      db.Word.findAll({
+        where: {
+          id: req.user.id
+        }
+      })
+        .then(function(data) {
+          res.json(data);
+        })
+        .catch(function(err) {
+          res.status(401).json(err);
+        });
+    }
+  });
+
+  app.post("/api/words", function(req, res) {
+    if (req.user) {
+      db.Word.create({
+        name: req.body.name,
+        language: req.body.language,
+        description: req.body.description,
+        image: req.body.image
+      })
+        .then(function(response) {
+          res.json(response);
+        })
+        .catch(function(err) {
+          res.status(401).json(err);
+        });
+    }
+  });
+
+  app.delete("/api/words/:id", function(req, res) {
+    if (req.user) {
+      db.Word.destroy({
+        where: { id: req.params.id }
+      }).then(function(response) {
+        res.json(response);
+      });
+    }
   });
 };
